fc42cdec2a0900e61cb6fadc6e05ad8b22b5b970
Overflow_Error <linmohan0814@163.com>
2025-05-17 01:14:56 +0800

    Create T609493.cpp

---
+++ code/T609493.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //非AC
+    long long a;
+    cin >> a;
+    if (a == 1) cout << 0 << endl;
+    else cout << (a + 1) / 2 << endl;
+    return 0;
+}
