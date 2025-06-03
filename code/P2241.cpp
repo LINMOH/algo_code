c54216f9dc5705feefcb6bd99be5bde253eaae50
Overflow_Error <linmohan0814@163.com>
2025-06-03 22:27:04 +0800

    Create P2241.cpp

---
+++ code/P2241.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    long long a = 0, b = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (i == j) {
+                a += (n - i + 1) * (m - j + 1);
+            } else {
+                b += (n - i + 1) * (m - j + 1);
+            }
+        }
+    }
+    cout << a << ' ' << b;
+}
